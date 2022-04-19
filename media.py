fd2a8bb32fd310198780d2fad6779b3c9f2ce83e
Tellybots <99309393+Tellybots@users.noreply.github.com>
2022-04-19 14:56:35 +0530

    Update media.py

--- media.py
+++ media.py
@@ -1,25 +1,27 @@
-import pyrogram
 
+import os
 import logging
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
+import logging.config
 
-from pyrogram import Client 
+# Get logging configurations
+logging.getLogger().setLevel(logging.ERROR)
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 from configs import Config
+from pyromod import listen
+from pyrogram import Client
 
-if __name__ == "__main__" :
-    plugins = dict(
-        root="dkbotz"
-    )
-    naysabots = Client(
-        "Metadata Extractor Bot",
-        bot_token=Config.BOT_TOKEN,
-        api_id=Config.API_ID,
-        api_hash=Config.API_HASH,
-        plugins=plugins,
-        workers=100
-    )
-    naysabots.run()
 
+def main():
+    dkbotz = dict(root="dkbotz")
+    app = Client("Media-info-Bot",
+                 bot_token=Config.BOT_TOKEN,
+                 api_id=Config.API_ID,
+                 api_hash=Config.API_HASH,
+                 plugins=dkbotz,
+                 workers=100)
+
+    app.run()
+
+
+if __name__ == "__main__":
+    main()
